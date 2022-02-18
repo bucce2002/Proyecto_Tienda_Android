efc3801a01bb9f2366d9675d3f17dbd7ddfdf3fd
Jesus <jesusbuccejb@gmail.com>
2022-02-18 15:52:04 +0100

    v1_creacion

---
+++ app/src/main/java/pildorasinformaticas/com/mylogin/proyecto_android_tienda_informatica/MainActivity.java
@@ -0,0 +1,14 @@
+package pildorasinformaticas.com.mylogin.proyecto_android_tienda_informatica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
